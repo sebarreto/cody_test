870fff34dfb034a0030ae1c15b52a57aa0629b1a
Sebastian Barreto <seb.barreto@gmail.com>
2023-02-01 11:11:25 +0100

    fix bug

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''This is a first test of Python and Github'''
 
 def onemethod():
-    print ("Hello I'm a method")
+    print ("Hi I'm a method")
 
 def anothermethod():
     print ("Hello I'm also a method")
